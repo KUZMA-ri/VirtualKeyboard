60a4231d1e5db58509ab5ee2b8c6422dcab61424
Marina <maryna_kuzmiankova@mail.ru>
2023-04-30 13:12:58 +0300

    feat: create DOM elements

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 "use strict";
 import { keyList } from "./scripts/keyList.js";
+import { createElement } from "./scripts/createElement.js";
+
+console.log(keyList);
+
+const body = document.querySelector('body');
+const container = createElement('div', 'container');
+const keyboardWrapper = createElement('div', 'keyboard__wrapper');
+const title  = createElement('h1','title','Virtual Keyboard');
+const descriptionWrapper = createElement('div', 'keyboard__desc-wrapper');
+const description = createElement('p', 'keyboard__desc', 'For changing language you have to use Shift + Alt')
+
+body.prepend(container);
+container.append(title, keyboardWrapper, descriptionWrapper);
+descriptionWrapper.append(description);
